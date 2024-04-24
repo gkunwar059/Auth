209e6554577360b96fd4554710d85c98787b62d9
Ganesh <gjkkunwar07@gmail.com>
2024-04-24 09:48:39 +0545

    fix comment minio

--- auth_system_Jwt_Token/file_uploader.py
+++ auth_system_Jwt_Token/file_uploader.py
@@ -1,56 +1,55 @@
-from minio import Minio
-from minio.error import S3Error
+# from minio import Minio
+# from minio.error import S3Error
 
 
-def main():
-    # create client with the MinIo server playground ,its access key
-    # and secret key
-    client = Minio(
-        "localhost:9000",
-        access_key="BlII7v7e8YGnJfJEopwC",
-        secure=False,
-        secret_key="HiWjT6Fhy9DeDhRETfOSHRPnNm8Rh9VwcfDcZkcv",
-    )
+# def main():
+#     # create client with the MinIo server playground ,its access key
+#     # and secret key
+#     client = Minio(
+#         "localhost:9000",
+#         access_key="BlII7v7e8YGnJfJEopwC",
+#         secure=False,
+#         secret_key="HiWjT6Fhy9DeDhRETfOSHRPnNm8Rh9VwcfDcZkcv",
+#     )
 
-    # the file to upload,change this path if needed
+#     # the file to upload,change this path if needed
 
-    source_file = "/home/ganesh/Downloads/ganesh.jpeg"
+#     source_file = "/home/ganesh/Downloads/ganesh.jpeg"
 
-    # the destination bucket and filename on the MinIo server
-    bucket_name = "test-bucket"
-    destination_file = "kunwar.jpeg"
+#     # the destination bucket and filename on the MinIo server
+#     bucket_name = "test-bucket"
+#     destination_file = "kunwar.jpeg"
 
-    # make the bucket if doesnot exits
-    found = client.bucket_exists(bucket_name)
-    if not found:
-        client.make_bucket(bucket_name)
-        print("Created bucket", bucket_name)
+#     # make the bucket if doesnot exits
+#     found = client.bucket_exists(bucket_name)
+#     if not found:
+#         client.make_bucket(bucket_name)
+#         print("Created bucket", bucket_name)
 
-    else:
-        print("Bucket", bucket_name, "already exits")
+#     else:
+#         print("Bucket", bucket_name, "already exits")
 
-    # upload the file ,remaining it in the process
-    client.fput_object(bucket_name, destination_file, source_file)
-    print(
-        source_file,
-        "successfully uploaded  as objects",
-        destination_file,
-        "to bucket",
-        bucket_name,
-    )
-
-
-if __name__ == "__main__":
-    try:
-        main()
-
-    except S3Error as exc:
-        print("error occurred", exc)
+#     # upload the file ,remaining it in the process
+#     client.fput_object(bucket_name, destination_file, source_file)
+#     print(
+#         source_file,
+#         "successfully uploaded  as objects",
+#         destination_file,
+#         "to bucket",
+#         bucket_name,
+#     )
 
 
-# ===========start the minio server =============================IMPORTANT TOPIC
-# sudo systemctl start minio.services
+# if __name__ == "__main__":
+#     try:
+#         main()
+
+#     except S3Error as exc:
+#         print("error occurred", exc)
+
+# # ===========start the minio server =============================IMPORTANT TOPIC
+# # sudo systemctl start minio.servicess
 
 
-# RESOURCES FOR THE FILE UPLOADER
-# https://min.io/docs/minio/linux/developers/python/minio-py.html
+# # RESOURCES FOR THE FILE UPLOADER
+# # https://min.io/docs/minio/linux/developers/python/minio-py.html
